ff8c49124a13395efe09a92a89b2ee6a1d250773
dbarkowsky <dylanbarkowsky@gmail.com>
2023-03-29 10:56:59 -0700

    added enemy model, animations, waypoint objs

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    //public float IdleTime { get; private set; } = 3.0f;         // time to spend in idle state
+    public float IdleTime { get; private set; } = 3.0f;         // time to spend in idle state
     //public float ChaseRange { get; private set; } = 10.0f;      // when player is closer than this, chase
     //public float AttackRange { get; private set; } = 7.0f;      // when player is closer than this, attack
     //public float AttackRangeStop { get; private set; } = 20.0f; // when player is farther than this, chase
@@ -13,27 +13,27 @@ public class Enemy : MonoBehaviour
     //public GameObject Player { get; private set; }
     //public NavMeshAgent Agent { get; private set; }
 
-    //public List<Transform> Waypoints { get; private set; }      // waypoints for patrol state
-    //private int waypointIndex = 0;                              // current waypoint index
+    public List<Transform> Waypoints { get; private set; }      // waypoints for patrol state
+    private int waypointIndex = 0;                              // current waypoint index
 
     //[SerializeField] private GameObject projectilePrefab;       // for creating "bullets"
     //[SerializeField] public Transform projectileSpawnPt;        // spawn point for bullets    
     //private float projectileForce = 35f;                        // force to shoot the projectile with
 
-    //void Start()
-    //{
-    //     Agent = GetComponent<NavMeshAgent>();                   // get a reference to the NavMeshAgent
-    //     Agent.updateUpAxis = false;
-    //     Player = GameObject.FindGameObjectWithTag("Player");    // get a reference to the Player
+    void Start()
+    {
+        //Agent = GetComponent<NavMeshAgent>();                   // get a reference to the NavMeshAgent
+        //Agent.updateUpAxis = false;
+        //Player = GameObject.FindGameObjectWithTag("Player");    // get a reference to the Player
 
-    //     // Create and populate a list of waypoints
-    //     Waypoints = new List<Transform>();
-    //     GameObject waypointsParent = GameObject.FindGameObjectWithTag("Waypoints");
-    //     foreach (Transform t in waypointsParent.transform)
-    //     {
-    //         Waypoints.Add(t);
-    //     }
-    // }
+        // Create and populate a list of waypoints
+        Waypoints = new List<Transform>();
+        GameObject waypointsParent = GameObject.FindGameObjectWithTag("Waypoints");
+        foreach (Transform t in waypointsParent.transform)
+        {
+            Waypoints.Add(t);
+        }
+    }
 
     //private void OnDrawGizmos()
     //{
@@ -47,17 +47,17 @@ public class Enemy : MonoBehaviour
     //    return Vector3.Distance(transform.position, Player.transform.position);
     //}
 
-    //public void DetermineNextWaypoint()
-    //{
-    //    pick a random waypoint
-    //    waypointIndex = Random.Range(0, Waypoints.Count);
-    //}
+    public void DetermineNextWaypoint()
+    {
+        // pick a random waypoint
+        waypointIndex = Random.Range(0, Waypoints.Count);
+    }
 
-    //public Vector3 GetCurrentWaypoint()
-    //{
-    //    return the current waypoint
-    //    return Waypoints[waypointIndex].position;
-    //}
+    public Vector3 GetCurrentWaypoint()
+    {
+        // return the current waypoint
+        return Waypoints[waypointIndex].position;
+    }
 
     // This method is triggered by the shooting animation
     //public void ShootEvent()
